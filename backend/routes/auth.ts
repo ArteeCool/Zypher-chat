89e907e05708102d0e331fe1d46e143073209a31
ArteeCool <84199800+ArteeCool@users.noreply.github.com>
2025-04-22 15:58:32 +0300

    First commit

---
+++ backend/routes/auth.ts
@@ -0,0 +1,64 @@
+import express from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import User from "../scemas/user.scema.ts";
+
+const router = express.Router();
+
+const JWT_SECRET = process.env.JWT_SECRET || "your_secret_key";
+
+router.post("/register", async (req, res) => {
+  try {
+    const { email, username, password } = req.body;
+
+    const userByEmail = await User.findOne({ email });
+    const userByUsername = await User.findOne({ username });
+
+    if (userByEmail || userByUsername) {
+      return res.status(400).json({ msg: "User already exists" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+
+    res.status(201).json({ msg: "User registered successfully" });
+  } catch (error) {
+    console.error("Register error:", error);
+    res.status(500).json({ msg: "Server error", data: error });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) return res.status(400).json({ msg: "User not found" });
+
+    if (user.password) {
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) return res.status(400).json({ msg: "Invalid credentials" });
+
+      const token = jwt.sign({ id: user._id }, JWT_SECRET, {
+        expiresIn: "1h",
+      });
+
+      res.json({ token });
+    }
+  } catch (error) {
+    console.error("Login error:", error);
+    res.status(500).json({ msg: "Server error" });
+  }
+});
+
+export default router;
---
+++ backend/server.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+import authRoutes from "./routes/auth.ts";
+
+// Config
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`);
+});
+
+mongoose.connect(
+  `mongodb+srv://artemgawrilyuk:${process.env.MONGODB_PASSWORD}@cluster0.c6zcwiu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+);
+
+app.use("/api/auth", authRoutes);
+
+import auth from "./middleware/auth.ts";
+
+app.get("/api/protected", auth, (req, res) => {
+  res.json({ msg: `Hello user ${req.user.id}` });
+});
