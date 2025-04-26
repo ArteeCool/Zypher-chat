0d315050d9d5309a6d74affa46b0f6b3ef537afb
ArteeCool <84199800+ArteeCool@users.noreply.github.com>
2025-04-26 18:21:47 +0300

    Fix

--- backend/scemas/chat.scema.ts
+++ backend/scemas/chat.scema.ts
@@ -1,19 +1,16 @@
 import mongoose from "mongoose";
 
-const chatSchema = new mongoose.Schema(
-  {
-    participantsIds: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-    messages: [
-      {
-        senderId: { type: mongoose.Schema.Types.ObjectId, required: true },
-        text: { type: String, required: true },
-      },
-    ],
-  },
-  {
-    collection: "Chats",
-  }
-);
+const chatSchema = new mongoose.Schema({
+  participantsIds: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  messages: [
+    {
+      senderId: { type: mongoose.Schema.Types.Mixed, required: true },
+      text: { type: String, required: true },
+      timestamp: { type: Date, default: Date.now },
+    },
+  ],
+});
 
 const Chat = mongoose.model("Chat", chatSchema);
+
 export default Chat;
